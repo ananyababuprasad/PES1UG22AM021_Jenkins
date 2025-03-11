b374b23404319f02ad60b388ee92d2502d3900f2
Ananya B <143985071+ananyababuprasad@users.noreply.github.com>
2025-03-11 16:48:49 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, this is main.cpp\n";
+  cout << "I am Ananya B\n";
+  cout << "I have successfully built and run this!!! \n";
+  return 0;
+}
